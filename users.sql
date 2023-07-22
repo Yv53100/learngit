7d91ed8aa7aaaaa3a966be7f44dfac5308a6ec3e
Ecazy <yv53100@outlook.com>
2023-07-23 00:52:02 +0800

    try

--- users.sql
+++
@@ -1,6 +0,0 @@
-
-USE user;
-CREATE TABLE IF NOT EXISTS app01_userinfo(id  INT PRIMARY KEY AUTO_INCREMENT,user_name VARCHAR(32),password VARCHAR(64),account VARCHAR(20));
-INSERT INTO app01_userinfo VALUES (1,'张三','123456','1234567890');
-INSERT INTO app01_userinfo VALUES (2,'李四','654321','2345678901');
-
